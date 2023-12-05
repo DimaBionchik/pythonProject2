8665bdf9fd825a58713899d97a3c9929eb80bf05
mac <bionchikk@mail.ru>
2023-12-05 14:19:46 +0300

    new exersice №1

--- Exercise1.py
+++ Exercise1.py
@@ -1,13 +1,6 @@
-# a,b - стороны отверстия
-# с,d - стороны кирпича
-
-
-def area(a,b,c,d):
-    area_hole = a*b
-    area_brick = c*d
-    if(area_hole>=area_brick):
-        return True
-    else:
-        return False
-
-print(area(4,5,4,5))
+def perimetr (char, number ):
+    result=0
+    if char =="s" :
+        return number*4
+    elif char =='c':
+        return number*0.5
